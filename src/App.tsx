c392d65f2f7536a09d3c074c34eb53b1226c656b
Marcus Piri <zerogranit@hotmail.se>
2023-01-03 14:24:13 +0100

    fixed a get req

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import axios from 'axios';
+
+const click = async () => {
+    const res = await axios.get(
+        `${import.meta.env.VITE_URL_KEY}recipes/716429/information?apiKey=${
+            import.meta.env.VITE_API_KEY
+        }&includeNutrition=true.`
+    );
+    // console.log(res.data);
+    console.log(res.data);
+    return res;
+};
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <button onClick={click}>click</button>
+        </div>
+    );
 }
 
 export default App;
