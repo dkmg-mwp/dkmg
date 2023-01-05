6277515ac784f9795cd6c19b18ab1969f540b51b
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-05 14:41:06 +0100

    Added icons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,12 @@
+import { RiHome5Fill, RiSearchLine } from 'react-icons/ri';
+
 const Navbar = () => {
-    return <footer>Navbar</footer>;
+    return (
+        <footer>
+            <RiHome5Fill />
+            <RiSearchLine />
+        </footer>
+    );
 };
 
 export default Navbar;
