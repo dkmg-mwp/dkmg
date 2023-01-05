ac36b98bde677909fdc6d73df4457e219c1ac6de
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-05 15:04:40 +0100

    Added navlinks with paths and colors for active/inactive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,25 @@
 import { RiHome5Fill, RiSearchLine } from 'react-icons/ri';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <footer>
-            <RiHome5Fill />
-            <RiSearchLine />
+            <NavLink
+                to='/'
+                style={({ isActive }) => ({
+                    color: isActive ? '#EF8A62' : '#2B8872',
+                })}
+            >
+                <RiHome5Fill />
+            </NavLink>
+            <NavLink
+                to='/search'
+                style={({ isActive }) => ({
+                    color: isActive ? '#EF8A62' : '#2B8872',
+                })}
+            >
+                <RiSearchLine />
+            </NavLink>
         </footer>
     );
 };
