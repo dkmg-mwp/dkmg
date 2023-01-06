53f602f0bb28960b16112a86e05d82d962837427
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-06 16:37:11 +0100

    Set up for icon effects with styled components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,21 @@
-import { RiHome5Fill, RiSearchLine } from 'react-icons/ri';
-import { NavLink } from 'react-router-dom';
-import { NavbarContainer, Footer } from './Navbar.styles';
+import {
+    NavbarContainer,
+    Footer,
+    NavLinks,
+    HomeIcon,
+    SearchIcon,
+} from './Navbar.styles';
 
 const Navbar = () => {
     return (
         <Footer>
             <NavbarContainer>
-                <NavLink
-                    to='/'
-                    style={({ isActive }) => ({
-                        color: isActive ? '#EF8A62' : '#2B8872',
-                    })}
-                >
-                    <RiHome5Fill style={{ height: '24px', width: '24px' }} />
-                </NavLink>
-                <NavLink
-                    to='/search'
-                    style={({ isActive }) => ({
-                        color: isActive ? '#EF8A62' : '#2B8872',
-                    })}
-                >
-                    <RiSearchLine style={{ height: '24px', width: '24px' }} />
-                </NavLink>
+                <NavLinks to='/'>
+                    <HomeIcon></HomeIcon>
+                </NavLinks>
+                <NavLinks to='/search'>
+                    <SearchIcon></SearchIcon>
+                </NavLinks>
             </NavbarContainer>
         </Footer>
     );
