61b11156226490050bcace22f0a93a036bc5dd5c
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-06 09:55:54 +0100

    DKMG-77 Added state and props

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 const Home = () => {
+    const [food, setFood] = useState<Food[]>([]);
     return (
         <div>
             <h1>
                 Friends with allergies? No worries, we’ll help you not to get
                 them killed..
             </h1>
-            <SearchBar />
+            <SearchBar setFood={setFood} />
         </div>
     );
 };
