9d3d0be70b99526eac1417c14558b94659aaa223
Marcus Piri <zerogranit@hotmail.se>
2023-01-06 09:36:06 +0100

    renamed css files and fixed error page

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,36 +1,22 @@
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom';
 
-const ErrorPage = () => {
-    const error = useRouteError();
-
-    if (isRouteErrorResponse(error)) {
-        if (error.status === 401) {
-            // ...
-        } else if (error.status === 404) {
-            // ...
-        }
-
-        return (
-            <div id='error-page'>
-                <h1>Oops! {error.status}</h1>
-                <p>{error.statusText}</p>
-                {error.data?.message && (
-                    <p>
-                        <i>{error.data.message}</i>
-                    </p>
-                )}
-            </div>
-        );
-    } else if (error instanceof Error) {
-        <div id='error-page'>
-            <h1>Oops! Unexpected Error</h1>
-            <p>Something went wrong.</p>
-            <p>
-                <i>{error.message}</i>
-            </p>
-        </div>;
-    } else {
-        return <></>;
-    }
+type Err = {
+    status: number;
+    statusText: string;
+    message: string;
 };
-export default ErrorPage;
+
+export default function ErrorPage() {
+    const error = useRouteError() as Err;
+
+    return (
+        <div id='error-page'>
+            <h1>Oops! {error.status}</h1>
+            <p>{error.statusText}</p>
+
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+        </div>
+    );
+}
---
+++ src/routes/Search/Search.styles.tsx
@@ -0,0 +1,2 @@
+import styled from 'styled-components';
+const Container = styled.div``;
