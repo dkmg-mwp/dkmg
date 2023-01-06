aca02f67b8eaadef731d618483d7b6cbabb2dc1a
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-06 13:57:37 +0100

    Fixed issue with background overlaps and searchbar position

--- src/routes/Home/Home.styles.tsx
+++ src/routes/Home/Home.styles.tsx
@@ -27,14 +27,19 @@ export const Container = styled.section`
 `;
 
 export const SearchBarSection = styled.section`
+    box-sizing: border-box;
+    width: 280px;
+    height: 64px;
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 20px 32px;
-    background: rgba(255, 255, 255, 0.63);
     position: absolute;
-    width: 280px;
-    left: 20px;
     top: 302px;
+    left: 20px; 
+    background: rgba(255, 255, 255, 0.63);
+    border: 1px solid #dde5e9;
+    box-shadow: 0px 16px 17px rgba(128, 174, 152, 0.23);
+    backdrop-filter: blur(10.5px);
     border-radius: 40px;
 `;
