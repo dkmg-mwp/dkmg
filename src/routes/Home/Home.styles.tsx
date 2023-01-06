8ef4cdc054deea38bc4ef1f2ce88610acdb86274
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-06 14:11:24 +0100

    Changed order of styles to reflect the component and formatting

--- src/routes/Home/Home.styles.tsx
+++ src/routes/Home/Home.styles.tsx
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
+export const Container = styled.section`
+    position: relative;
+    min-width: 100vw;
+    min-height: 100vh;
+    top: 0px;
+    background: #cce3c8;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
 export const Title = styled.h1`
     position: relative;
     width: 280px;
@@ -17,21 +32,6 @@ export const Title = styled.h1`
     color: #2b8876;
 `;
 
-export const Wrapper = styled.div`
-    display: flex; 
-    flex-direction: column; 
-    justify-content: center; 
-    align-items: center; 
-`
-
-export const Container = styled.section`
-    position: relative;
-    min-width: 100vw;
-    min-height: 100vh;
-    top: 0px;
-    background: #cce3c8;
-`;
-
 export const SearchBarSection = styled.section`
     box-sizing: border-box;
     width: 280px;
@@ -42,7 +42,7 @@ export const SearchBarSection = styled.section`
     padding: 20px 32px;
     position: relative;
     top: 132px;
-    left: 0px; 
+    left: 0px;
     background: rgba(255, 255, 255, 0.63);
     border: 1px solid #dde5e9;
     box-shadow: 0px 16px 17px rgba(128, 174, 152, 0.23);
