e56a20d543e2f4d7ef591a216dc5e0c9b27fbd9d
Sandy <sandy.congreve@cmeducations.se>
2023-01-08 20:32:36 +0100

    Added globalstyles file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import GlobalStyle from './components/styles/Globalstyles.styled';
 
 function App() {
     return (
         <div className='App'>
             <Outlet />
             <Navbar />
+            <GlobalStyle/>
         </div>
     );
 }
