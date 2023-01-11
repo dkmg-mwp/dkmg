670e5f615c82bfe1e877b305f41043d7de01ad0e
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-11 15:58:27 +0100

    Added list options for guest

--- src/routes/User/User.tsx
+++ src/routes/User/User.tsx
@@ -7,10 +7,30 @@ import { addGuest, fetchGuests, removeGuest } from '../../api/dkmg-api';
 const User = () => {
     const [guests, setGuests] = useState<Guest[]>([]);
     const [input, setInput] = useState('');
+    const [dairyFree, setDairyFree] = useState(false);
+    const [glutenFree, setGlutenFree] = useState(false);
+    const [vegan, setVegan] = useState(false);
+    const [vegetarian, setVegetarian] = useState(false);
+
+    const handleDairyFree = () => {
+        setDairyFree((prev) => !prev);
+    };
+
+    const handleGlutenFree = () => {
+        setGlutenFree((prev) => !prev);
+    };
+
+    const handleVegan = () => {
+        setVegan((prev) => !prev);
+    };
+
+    const handleVegetarian = () => {
+        setVegetarian((prev) => !prev);
+    };
 
     const handleAdd = async (name: string) => {
         if (input.length === 0) return;
-        await addGuest(name);
+        await addGuest(name, dairyFree, glutenFree, vegan, vegetarian);
         setInput('');
         await fetchGuests().then(setGuests);
     };
@@ -38,7 +58,7 @@ const User = () => {
         document.title = 'User';
         fetchGuests().then(setGuests);
     }, []);
-    
+
     return (
         <Container>
             <Wrapper>
@@ -48,16 +68,59 @@ const User = () => {
 
                 <InnerContainer>
                     <InputSection>
-                        <Input
-                            type='text'
-                            value={input}
-                            placeholder='Enter name'
-                            onChange={(e) => setInput(e.target.value)}
-                        />
-                        <RiUserAddFill
-                            onClick={() => handleAdd(input)}
-                            size={25}
-                        />
+                        <form onSubmit={(e) => e.preventDefault()}>
+                            <Input
+                                type='text'
+                                value={input}
+                                placeholder='Enter name'
+                                onChange={(e) => setInput(e.target.value)}
+                            />
+                            <div>
+                                <h4>Restrictions:</h4>
+                                <ul>
+                                    <li>
+                                        <input
+                                            type='checkbox'
+                                            name='Dairy Free'
+                                            value={dairyFree.toString()}
+                                            onChange={handleDairyFree}
+                                        />
+                                        <label>Dairy Free</label>
+                                    </li>
+                                    <li>
+                                        <input
+                                            type='checkbox'
+                                            name='Gluten Free'
+                                            value={glutenFree.toString()}
+                                            onChange={handleGlutenFree}
+                                        />
+                                        <label>Gluten Free</label>
+                                    </li>
+                                    <li>
+                                        <input
+                                            type='checkbox'
+                                            name='Vegan'
+                                            value={vegan.toString()}
+                                            onChange={handleVegan}
+                                        />
+                                        <label>Vegan</label>
+                                    </li>
+                                    <li>
+                                        <input
+                                            type='checkbox'
+                                            name='Vegetarian'
+                                            value={vegetarian.toString()}
+                                            onChange={handleVegetarian}
+                                        />
+                                        <label>Vegetarian</label>
+                                    </li>
+                                </ul>
+                            </div>
+                            <RiUserAddFill
+                                onClick={() => handleAdd(input)}
+                                size={25}
+                            />
+                        </form>
                     </InputSection>
                     {guestRender()}
                 </InnerContainer>
