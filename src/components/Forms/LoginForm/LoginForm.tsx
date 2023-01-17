4ee1fdd5cf1eb54a279ab1465ffdd272da157e76
Sandy <sandy.congreve@cmeducations.se>
2023-01-17 15:14:16 +0100

    Added function to login a user though login form

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,4 +1,6 @@
-import { useContext } from 'react';
+import axios from 'axios';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AccountContext } from '../../../routes/Login/Login.context';
 
 import {
@@ -10,18 +12,50 @@ import {
     SubmitButton,
 } from './LoginForm.styles';
 
-
-
 const LoginForm = () => {
     const { switchToSignUp } = useContext(AccountContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        const res = await axios.post(`https://dkmg.glitch.me/auth/login`, {
+            email: email,
+            password: password,
+        });
+        console.log(res);
+        setToken(res.data);
+        if (token) {
+            navigate('/profile');
+        } else {
+            navigate('/login');
+        }
+        console.log(token);
+    };
+
     return (
         <BoxContainer>
-            <FormContainer>
-                <Input type='email' placeholder='Email' />
-                <Input type='password' placeholder='Password' />
+            <FormContainer
+                onSubmit={(e) => {
+                    handleLogin(), e.preventDefault();
+                }}
+            >
+                <Input
+                    type='email'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <SubmitButton type='submit'>Log in</SubmitButton>
             </FormContainer>
             <MutedLink>Forget your password?</MutedLink>
-            <SubmitButton type='submit'>Log in</SubmitButton>
             <MutedLink>
                 {' '}
                 Don&apos;t have an account?{' '}
