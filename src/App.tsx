dbd1ad75cc6fd6bb1370207cbf66c685c49e7902
Sandy <sandy.congreve@cmeducations.se>
2023-01-18 14:58:49 +0100

    Imported new header component with logo included

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from './components/styles/Globalstyles.styled';
+import Header from './components/Header/Header';
 
 function App() {
     return (
         <div className='App'>
+            <Header/>
             <Outlet />
             <Navbar />
             <GlobalStyle />
