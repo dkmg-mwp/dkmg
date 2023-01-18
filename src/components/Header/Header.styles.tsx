bc1e914a39bcf69e511d344fedd4520ca436d731
Sandy <sandy.congreve@cmeducations.se>
2023-01-18 15:15:29 +0100

    Created new header folder in components

---
+++ src/components/Header/Header.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+    top: 0;
+    z-index: 1;
+    position: sticky;
+    background-color: transparent;
+`;
+
+export const HeaderWrapper = styled.div`
+    padding: 10px;
+    position: absolute;
+`;
+export const Logo = styled.img``;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import * as Styled from './Header.styles';
+
+const Header = () => {
+    return (
+        <Styled.HeaderContainer>
+            <Styled.HeaderWrapper>
+                <Styled.Logo
+                    src='https://i.ibb.co/q9MVrBb/MainLogo.png'
+                    alt='MainLogo'
+                    height='50px'
+                />
+            </Styled.HeaderWrapper>
+        </Styled.HeaderContainer>
+    );
+};
+
+export default Header;
+
+
