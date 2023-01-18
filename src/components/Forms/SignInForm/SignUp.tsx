3ed573fce2ac467249230c8a766cbfea9631aaa9
Sandy <sandy.congreve@cmeducations.se>
2023-01-18 10:56:46 +0100

    Moved axios post request up to Login Page

--- src/components/Forms/SignInForm/SignUp.tsx
+++ src/components/Forms/SignInForm/SignUp.tsx
@@ -1,7 +1,4 @@
-// import axios from 'axios';
-import axios from 'axios';
 import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { AccountContext } from '../../../routes/Login/Login.context';
 
 import {
@@ -13,41 +10,28 @@ import {
     SubmitButton,
 } from '../LoginForm/LoginForm.styles';
 
-const SignUpForm = () => {
+type Props = {
+    handleSubmit: (email: string, password: string) => void;
+};
+
+const SignUpForm = ({ handleSubmit }: Props) => {
     const { switchToLogIn } = useContext(AccountContext);
-    const [firstName, setFirstName] = useState('');
+    // const [firstName, setFirstName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [token, setToken] = useState('');
-    const navigate = useNavigate();
-
-    const handleSubmit = async () => {
-        const res = await axios.post(`https://dkmg.glitch.me/auth/register`, {
-            firstname: firstName,
-            email: email,
-            password: password,
-        });
-        setToken(res.data);
-        if (token) {
-            navigate('/profile');
-        } else {
-            navigate('/login');
-        }
-        console.log(token);
-    };
 
     return (
         <BoxContainer>
             <FormContainer
                 onSubmit={(e) => {
-                    handleSubmit(), e.preventDefault();
+                    handleSubmit(email, password), e.preventDefault();
                 }}
             >
-                <Input
+                {/* <Input
                     type='text'
                     placeholder='Full Name'
                     onChange={(e) => setFirstName(e.target.value)}
-                />
+                /> */}
                 <Input
                     type='email'
                     placeholder='Email'
