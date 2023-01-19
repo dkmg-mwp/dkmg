315a576262da313e99d618fb32bb0e13c5592729
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-19 15:02:58 +0100

    Added logout function and navigation from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,23 @@
+import { useLogin } from '../../routes/Login/Login.context';
 import * as Styled from './Header.styles';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const { token, setToken } = useLogin();
+    const navigate = useNavigate();
+
+    const handleLogo = () => {
+        navigate('/');
+    };
+
+    const handleLogIn = () => {
+        navigate('/login');
+    };
+
+    const handlelogOut = () => {
+        setToken(null);
+    };
+
     return (
         <Styled.HeaderContainer>
             <Styled.HeaderWrapper>
@@ -8,8 +25,14 @@ const Header = () => {
                     src='https://i.ibb.co/q9MVrBb/MainLogo.png'
                     alt='MainLogo'
                     height='50px'
+                    onClick={handleLogo}
                 />
             </Styled.HeaderWrapper>
+            {!token ? (
+                <button onClick={handleLogIn}>LogIn</button>
+            ) : (
+                <button onClick={handlelogOut}>LogOut</button>
+            )}
         </Styled.HeaderContainer>
     );
 };
