a689fc2c0ec544af546cfaea9d2403764082daff
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-20 14:35:04 +0100

    Made it possible to be logged in when refreshing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import Navbar from './components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from './components/styles/Globalstyles.styled';
 import Header from './components/Header/Header';
+import { useEffect } from 'react';
+import { useLogin } from './routes/Login/Login.context';
+import { useProfile } from './routes/Profile/Profile.context';
 
 function App() {
+    const { setToken, token } = useLogin();
+    const { fetchGuests } = useProfile();
+
+    useEffect(() => {
+        const LocalToken = localStorage.getItem('token');
+        if (LocalToken) {
+            setToken(LocalToken);
+        }
+        if (token) {
+            fetchGuests();
+        }
+    }, []);
+
     return (
         <div className='App'>
             <Header />
