a4c2634f1fd674b441982a373eb65a9243cd8706
Sandy <sandy.congreve@cmeducations.se>
2023-01-21 16:00:18 +0100

    Compiled all buttons to Small, Medium and Big

--- src/components/Buttons/Button.styles.tsx
+++ src/components/Buttons/Button.styles.tsx
@@ -13,38 +13,10 @@ export const SmallButton = styled.button<{ primary?: boolean }>`
     height: 37px;
 `;
 
-export const AddButton = styled.button`
+export const MediumButton = styled.button<{ bgColor?: string }>`
     border: none;
-    background-color: #2b8872;
-    text-transform: uppercase;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    letter-spacing: 0.025em;
-    border-radius: 20px;
-    padding: 10px 15px;
-    width: 100%;
-`;
-
-export const LogOutButton = styled.button`
-    border: none;
-    background-color: #ef8a62;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    letter-spacing: 0.025em;
-    border-radius: 20px;
-    padding: 10px 15px;
-    width: 100%;
-`;
-
-export const LogInButton = styled.button`
-    border: none;
-    background-color: #2b8872;
+    background-color: ${(props) =>
+        props.bgColor === '#2b8872' ? '#2b8872' : '#ef8a62'};
     color: white;
     display: flex;
     justify-content: space-between;
@@ -57,7 +29,9 @@ export const LogInButton = styled.button`
     width: 100%;
 `;
 
-export const SignUpPageButton = styled.button`
+export const BigButton = styled.button<{ bgColor?: string }>`
+    background: ${(props) =>
+        props.bgColor === '#2b8872' ? '#2b8872' : '#ef8a62'};
     width: 100%;
     padding: 11px 40%;
     color: #fff;
@@ -67,32 +41,6 @@ export const SignUpPageButton = styled.button`
     border-radius: 100px 100px 100px 100px;
     cursor: pointer;
     transition: all, 240ms ease-in-out;
-    background: rgb(239, 138, 98);
-    background: linear-gradient(
-        58deg,
-        rgb(239, 138, 98) 20%,
-        rgb(239, 138, 98) 100%
-    );
-    &:hover {
-        filter: brightness(1.03);
-    }
-`;
-export const LogInPageButton = styled.button`
-    width: 100%;
-    padding: 11px 40%;
-    color: #fff;
-    font-size: 15px;
-    font-weight: 600;
-    border: none;
-    border-radius: 100px 100px 100px 100px;
-    cursor: pointer;
-    transition: all, 240ms ease-in-out;
-    background: rgb(43, 136, 114);
-    background: linear-gradient(
-        58deg,
-        rgb(43, 136, 114) 20%,
-        rgb(43, 136, 114) 100%
-    );
     &:hover {
         filter: brightness(1.03);
     }
