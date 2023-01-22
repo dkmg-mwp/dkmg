1a38ab06bdec5f8ba5b2481925313564b8fbba02
Sandy <sandy.congreve@cmeducations.se>
2023-01-23 00:22:43 +0100

    Refactored code to have less ternaries and perform same actions with less code

--- src/components/AllergyList/AllergyList.tsx
+++ src/components/AllergyList/AllergyList.tsx
@@ -3,71 +3,56 @@ import { RiPlantFill, RiPlantLine } from 'react-icons/ri';
 import { TbMilk } from 'react-icons/tb';
 import * as Styled from './AllergyList.styles';
 
+type TagProps = {
+    type: boolean;
+    children: React.ReactNode;
+};
+const Tag = ({ type, children }: TagProps) => {
+    const TagStyle = type ? Styled.IncludeTag : Styled.ExcludeTag;
+    return <TagStyle>{children}</TagStyle>;
+};
+
 type Props = {
     dish: Dish;
 };
 
 const AllergyList = ({ dish }: Props) => {
+    const allergies = [
+        {
+            name: 'dairy free',
+            icon: <TbMilk size={30} />,
+            prop: dish.dairyFree,
+        },
+        {
+            name: 'gluten free',
+            icon: <CiWheat size={30} />,
+            prop: dish.glutenFree,
+        },
+        {
+            name: 'vegan safe',
+            icon: <RiPlantFill size={30} />,
+            prop: dish.vegan,
+        },
+        {
+            name: 'vegetarian safe',
+            icon: <RiPlantLine size={30} />,
+            prop: dish.vegetarian,
+        },
+    ];
+
     return (
-        <Styled.CardAccordion>
             <Styled.CardAccordionBox>
-                <Styled.CardAccordionInfo>
-                    {dish.dairyFree ? (
-                        <Styled.IncludeTag>
-                            {' '}
-                            <TbMilk size={30} />
-                            Dairy Free: Yes
-                        </Styled.IncludeTag>
-                    ) : (
-                        <Styled.ExcludeTag>
-                            {' '}
-                            <TbMilk size={30} /> Dairy Free: No{' '}
-                        </Styled.ExcludeTag>
-                    )}
-                </Styled.CardAccordionInfo>
-                <Styled.CardAccordionInfo>
-                    {dish.glutenFree ? (
-                        <Styled.IncludeTag>
-                            {' '}
-                            <CiWheat size={30} /> Gluten Free: Yes
-                        </Styled.IncludeTag>
-                    ) : (
-                        <Styled.ExcludeTag>
-                            {' '}
-                            <CiWheat size={30} />
-                            Gluten Free: No
-                        </Styled.ExcludeTag>
-                    )}
-                </Styled.CardAccordionInfo>
-                <Styled.CardAccordionInfo>
-                    {dish.vegan ? (
-                        <Styled.IncludeTag>
-                            {' '}
-                            <RiPlantFill size={30} /> Vegan Safe: Yes
-                        </Styled.IncludeTag>
-                    ) : (
-                        <Styled.ExcludeTag>
-                            {' '}
-                            <RiPlantFill size={30} /> Vegan Safe: No
-                        </Styled.ExcludeTag>
-                    )}
-                </Styled.CardAccordionInfo>
-                <Styled.CardAccordionInfo>
-                    {dish.vegetarian ? (
-                        <Styled.IncludeTag>
-                            {' '}
-                            <RiPlantLine size={30} />
-                            Vegetarian Safe: Yes
-                        </Styled.IncludeTag>
-                    ) : (
-                        <Styled.ExcludeTag>
-                            {' '}
-                            <RiPlantLine size={30} /> Vegetarian Safe: No
-                        </Styled.ExcludeTag>
-                    )}
-                </Styled.CardAccordionInfo>
+                {allergies.map((allergy) => (
+                    <Styled.CardAccordionInfo key={allergy.name}>
+                        <Tag type={allergy.prop}>
+                            {allergy.icon}
+                            {allergy.prop
+                                ? `Is ${allergy.name}`
+                                : `Not ${allergy.name}`}
+                        </Tag>
+                    </Styled.CardAccordionInfo>
+                ))}
             </Styled.CardAccordionBox>
-        </Styled.CardAccordion>
     );
 };
 
