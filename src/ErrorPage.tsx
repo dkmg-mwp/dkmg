6d540766d38824fa6e18813e417d679448427c4b
Sandy <sandy.congreve@cmeducations.se>
2023-01-23 11:43:06 +0100

    Added styling in error page file for clarity

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -2,7 +2,25 @@ import { useRouteError } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import GlobalStyle from './components/styles/Globalstyles.styled';
 import Heading from './components/styles/Heading.styles';
-import * as Styled from './routes/Home/Home.styles';
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    min-width: 100vw;
+    min-height: 100vh;
+    top: 0px;
+    background: #cce3c8;
+    padding-top: 50px;
+`;
+export const TextContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 30px 40px 10px 40px;
+    flex-direction: column;
+`;
+export const Image = styled.img`
+    padding: 0.5rem;
+`;
 
 type Err = {
     status: number;
@@ -16,30 +34,30 @@ export default function ErrorPage() {
     console.log(error.message);
 
     return (
-        <Styled.Container id='error-page'>
-            <Styled.TextContainer>
+        <Container id='error-page'>
+            <TextContainer>
                 <Heading variant={'h1'}>
                     Oops! Seems like something went a bit potato..{' '}
                 </Heading>
-                <img
+                <Image
                     src='https://i.ibb.co/7bTnLj0/Error.png'
                     alt='error'
                     height='100px'
                 />
-            </Styled.TextContainer>
-            <Styled.TextContainer>
+            </TextContainer>
+            <TextContainer>
                 {' '}
                 <Heading variant={'h3'}>
                     But it is not your potato fault it is our...
                 </Heading>
-                <img
+                <Image
                     src='https://i.ibb.co/s1Qbt5T/Error-orange-face.png'
                     alt='Error-orange-face'
                     height='100px'
                 />
-            </Styled.TextContainer>
+            </TextContainer>
             <Navbar />
             <GlobalStyle />
-        </Styled.Container>
+        </Container>
     );
 }
