f26976ef3584aa61fc66c3ca46c7ed8306c972d7
Marcus Piri <zerogranit@hotmail.se>
2023-01-24 15:08:40 +0100

    useeffect

--- src/routes/Profile/Profile.context.tsx
+++ src/routes/Profile/Profile.context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { api } from '../../api/glitch/guest-api';
 import { useLogin } from '../Login/Login.context';
 
