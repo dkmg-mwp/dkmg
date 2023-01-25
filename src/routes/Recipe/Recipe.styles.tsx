9de5c3dc190d66ad0f8d8ee2ef22822baf144f9f
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:46:24 +0100

    Cleaned

--- src/routes/Recipe/Recipe.styles.tsx
+++ src/routes/Recipe/Recipe.styles.tsx
@@ -1,5 +1,6 @@
 import { RiGroupFill, RiTimeFill } from 'react-icons/ri';
 import styled from 'styled-components';
+
 export const Container = styled.div`
     min-width: 100vw;
     padding-top: 50px;
@@ -7,18 +8,25 @@ export const Container = styled.div`
 `;
 
 export const Wrapper = styled.section`
-    border-radius: 26px 26px 0px 0px;
-    margin-top: 20px;
-    width: 100%;
-    height: 100%;
     display: flex;
     justify-content: center;
     flex-direction: column;
+    height: 100%;
+    width: 100%;
     gap: 1rem;
-    background: #cce3c8;
     padding: 2rem;
+    margin-top: 20px;
     padding-bottom: 150px;
+    border-radius: 26px 26px 0px 0px;
+    background: #cce3c8;
 `;
+
+export const Image = styled.img`
+    height: 200px;
+    width: 100%;
+    border-radius: 10px;
+`;
+
 export const Recipe = styled.section`
     display: flex;
     justify-content: center;
@@ -26,50 +34,49 @@ export const Recipe = styled.section`
     gap: 2rem;
 `;
 
-export const Image = styled.img`
-    width: 100%;
-    height: 200px;
-    border-radius: 10px;
-`;
 export const FirstSection = styled.section`
     display: flex;
     flex-direction: column;
-    background-color: #eff2ee;
-    border-radius: 10px;
     gap: 1rem;
     padding: 2rem;
+    border-radius: 10px;
+    background-color: #eff2ee;
 `;
+
 export const InfoContainer = styled.div`
     display: flex;
-    align-items: flex-start;
-    gap: 4rem;
     align-items: center;
+    gap: 4rem;
     padding-bottom: 1rem;
 `;
+
 export const Time = styled.div`
     display: flex;
-    gap: 1rem;
     align-items: center;
+    gap: 1rem;
 `;
+
 export const TimeIcon = styled(RiTimeFill)`
-    color: #ef8a62;
     font-size: 24px;
+    color: #ef8a62;
 `;
+
 export const Servings = styled.div`
     display: flex;
-    gap: 1rem;
     align-items: center;
+    gap: 1rem;
 `;
+
 export const ServingIcon = styled(RiGroupFill)`
-    color: #ef8a62;
     font-size: 24px;
+    color: #ef8a62;
 `;
 
 export const Ingredients = styled.ul`
     display: flex;
-    padding-top: 1rem;
     flex-direction: column;
     gap: 0.5rem;
+    padding-top: 1rem;
     margin-bottom: 0.3rem;
 `;
 
@@ -78,18 +85,19 @@ export const Ingredient = styled.li`
     justify-content: flex-start;
     gap: 0.5rem;
 `;
-export const Amount = styled.h6<{ color?: string, weight?: number }>`
+
+export const Amount = styled.h6<{ color?: string; weight?: number }>`
     color: ${(props) => (props.color === '#2b8872' ? '#2b8872' : '#ef8a62')};
-    font-weight: ${(props) => (props.weight === 800 ? 800 : 600 )};
+    font-weight: ${(props) => (props.weight === 800 ? 800 : 600)};
     display: flex;
     align-items: center;
 `;
 
 export const Instructions = styled.ol`
-    padding-left: 1rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    padding-left: 1rem;
 `;
 
 export const Step = styled.li``;
