ca6b2f30dc42d19077428b572ce22e99eebcf75d
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:50:56 +0100

    changed .styled to .styles

---
+++ src/components/styles/Globalstyles.styles.tsx
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    font-family: Mulish;
+    font-size: 14px;
+    line-height: 24px;
+    font-weight: 400;
+    color-scheme: light dark;
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+    box-sizing: border-box;
+}
+
+ul {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    min-width: 320px;
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    background-color: #ffffff;
+    color: #2b8872;
+}
+`;
+
+export default GlobalStyle;
