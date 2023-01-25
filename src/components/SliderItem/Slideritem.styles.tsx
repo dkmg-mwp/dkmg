759b3e74ae43c86186c08ab726943b369c0d936a
Sandy <sandy.congreve@cmeducations.se>
2023-01-25 09:43:40 +0100

    Added padding bottom to home

--- src/components/SliderItem/Slideritem.styles.tsx
+++ src/components/SliderItem/Slideritem.styles.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
-export const CardContainer = styled.div<{ zIndex?: number }>`
-    z-index: ${(props) => (props.zIndex ? -190 : 180)};
+export const CardContainer = styled.div`
     display: flex;
     flex-direction: column;
     background: #e8f6e385;
