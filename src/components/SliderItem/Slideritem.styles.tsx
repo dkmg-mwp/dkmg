8f4c66019e426dc2bdfbbc93626136ab388b164a
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:23:54 +0100

    Cleaned

--- src/components/SliderItem/Slideritem.styles.tsx
+++ src/components/SliderItem/Slideritem.styles.tsx
@@ -2,24 +2,25 @@ import styled from 'styled-components';
 
 export const CardContainer = styled.div`
     display: flex;
-    flex-direction: column;
-    background: #e8f6e385;
-    width: 200px;
-    margin-bottom: 20px;
-    box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
-    backdrop-filter: blur(2px);
-    border-radius: 15px;
     justify-content: space-between;
-    gap: 10px;
+    flex-direction: column;
     height: auto;
+    width: 200px;
+    gap: 10px;
+    margin-bottom: 20px;
+    border-radius: 15px;
+    box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
+    background: #e8f6e385;
+    backdrop-filter: blur(2px);
 `;
 
 export const CardImage = styled.img`
-    width: 200px;
     height: 150px;
+    width: 200px;
     object-fit: cover;
     border-radius: 15px 15px 0px 0px;
 `;
+
 export const CardColumnTop = styled.div`
     max-width: 100%;
     padding: 0.1rem 1rem;
