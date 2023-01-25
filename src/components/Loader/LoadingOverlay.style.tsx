2828a6f987852361cc6ee657b0628716c146360f
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:12:16 +0100

    Cleaned

--- src/components/Loader/LoadingOverlay.style.tsx
+++ src/components/Loader/LoadingOverlay.style.tsx
@@ -1,14 +1,14 @@
 import styled, { keyframes } from 'styled-components';
 
 export const LoadingContainer = styled.div`
-    position: fixed;
-    min-width: 100vw;
-    min-height: 100vh;
-    z-index: 1000;
-    inset: 0 0 0 0;
     display: flex;
+    position: fixed;
     justify-content: center;
     align-items: center;
+    min-height: 100vh;
+    min-width: 100vw;
+    z-index: 1000;
+    inset: 0 0 0 0;
     background-color: #CCE3C8;
 `;
 
@@ -26,4 +26,5 @@ export const Circle = styled.img`
     width: 65px;
     height: 65px;
     border-radius: 50%;
-`;
+    `;
+
