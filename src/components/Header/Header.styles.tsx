d4e4db332aa27a6bd0ac206ad4550bce9cc90d01
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:08:15 +0100

    Cleaned

--- src/components/Header/Header.styles.tsx
+++ src/components/Header/Header.styles.tsx
@@ -1,24 +1,24 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 10px;
     top: 0;
     z-index: 2;
     position: absolute;
     background-color: transparent;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    align-items: center;
 `;
 
 export const HeaderWrapper = styled.div``;
+
 export const Logo = styled.img``;
 
 export const Prompt = styled.div`
     position: fixed;
-    backdrop-filter: blur(8px);
-    bottom: 0px;
-
     height: 100vh;
+    bottom: 0px;
+    backdrop-filter: blur(8px);
 `;
