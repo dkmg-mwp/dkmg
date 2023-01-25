40587acc9673b2071f9928a49945b234fd62638b
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:31:26 +0100

    Cleaned

--- src/routes/Home/Home.styles.tsx
+++ src/routes/Home/Home.styles.tsx
@@ -3,27 +3,28 @@ import styled from 'styled-components';
 export const Container = styled.section`
     min-width: 100vw;
     min-height: 100vh;
-    top: 0px;
-    background: #cce3c8;
     padding-top: 50px;
     padding-bottom: 75px;
+    top: 0px;
+    background: #cce3c8;
 `;
+
 export const TextContainer = styled.div`
     display: flex;
     align-items: flex-start;
-    padding: 20px 40px 20px 20px;
     flex-direction: column;
+    padding: 20px 40px 20px 20px;
 `;
 
 export const SliderSection = styled.div`
-    position: relative;
     display: flex;
     align-items: center;
     padding: 20px 0px 20px 20px;
+    position: relative;
 `;
 
 export const Slider = styled.div`
     display: flex;
-    overflow-x: auto;
     gap: 2rem;
+    overflow-x: auto;
 `;
