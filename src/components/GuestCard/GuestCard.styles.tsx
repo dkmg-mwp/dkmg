4f932d5087af5f3ce63f8e34977208dc08f5922b
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:53:35 +0100

    changed name from .style to .styles

---
+++ src/components/GuestCard/GuestCard.styles.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    height: auto;
+    width: 100%;
+    gap: 10px;
+    padding: 20px 20px;
+    margin-bottom: 20px;
+    border-radius: 15px;
+    box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
+    background: #eff2ee;
+    backdrop-filter: blur(2px);
+`;
+
+export const CardWrapper = styled.div`
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
+    gap: 5.3rem;
+`;
+
+export const CardColumnLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+export const CardImage = styled.img`
+    max-width: 75px;
+    border-radius: 15px;
+    object-fit: cover;
+`;
+
+export const CardColumnRight = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+    gap: 50px;
+`;
