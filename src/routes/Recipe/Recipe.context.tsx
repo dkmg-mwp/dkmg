e8afb45fd56beecfc8e2d952a525995a4223e84d
Linda Andersson <linda.29.andersson@gmail.com>
2023-01-25 10:13:52 +0100

    Created context for recipe

---
+++ src/routes/Recipe/Recipe.context.tsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from 'react';
+
+const RecipeContext = createContext<RecipeContext | null>(null);
+
+export const RecipeProvider = ({ children }: ProviderProps) => {
+    const [randomRecipe, setRandomRecipe] = useState<Recipe | null>(null);
+    const [recipe, setRecipe] = useState<Recipe | null>(null);
+
+    return (
+        <RecipeContext.Provider
+            value={{
+                randomRecipe,
+                setRandomRecipe,
+                recipe,
+                setRecipe,
+            }}
+        >
+            {children}
+        </RecipeContext.Provider>
+    );
+};
+
+export const useRecipe = () => {
+    const contextValue = useContext(RecipeContext);
+    if (!contextValue) {
+        throw new Error('useRecipe is outside RecipeProvider');
+    }
+    return contextValue;
+};
