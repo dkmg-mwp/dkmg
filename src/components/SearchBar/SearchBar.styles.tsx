4aaf4eaccb17ed5b383345a7f5609a1ec15cae80
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:21:53 +0100

    Cleaned

--- src/components/SearchBar/SearchBar.styles.tsx
+++ src/components/SearchBar/SearchBar.styles.tsx
@@ -1,29 +1,28 @@
 import styled from 'styled-components';
 
 export const SearchBarContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     box-sizing: border-box;
     width: 100%;
     height: 64px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
     padding: 20px 32px;
-    background: rgba(255, 255, 255, 0.63);
     border: 1px solid #dde5e9;
-    box-shadow: 0px 16px 17px rgba(128, 174, 152, 0.23);
-    backdrop-filter: blur(10.5px);
     border-radius: 40px;
+    backdrop-filter: blur(10.5px);
+    box-shadow: 0px 16px 17px rgba(128, 174, 152, 0.23);
+    background: rgba(255, 255, 255, 0.63);
 `;
 
 export const SearchBarInput = styled.input`
-    text-decoration: none;
     border-style: none;
+    text-decoration: none;
     background-color: transparent;
     color: #2b8872;
     font-family: Mulish;
     &:focus {
         outline: none;
         color: #2b8872;
-        /* box-shadow: 0px 0px 2px red; */
     }
 `;
