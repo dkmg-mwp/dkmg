89627e829c3a604698fa76553646ce3412e7c98a
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:06:02 +0100

    Cleaned

--- src/components/GuestFilter/GuestFilter.styles.tsx
+++ src/components/GuestFilter/GuestFilter.styles.tsx
@@ -3,18 +3,18 @@ import styled from 'styled-components';
 export const CardContainer = styled.div`
     display: flex;
     justify-content: space-evenly;
-    background: #eff2ee;
-    box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
-    backdrop-filter: blur(2px);
     border-radius: 15px;
     padding: 5px;
     margin: 5px;
+    box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
+    background: #eff2ee;
+    backdrop-filter: blur(2px);
 `;
 
 export const Checkbox = styled.input`
-    accent-color: orange;
     border-radius: 0.15em;
-    background-color: #fff;
-    color: currentColor;
     border: 0.15em solid currentColor;
+    color: currentColor;
+    accent-color: orange;
+    background-color: #fff;
 `;
