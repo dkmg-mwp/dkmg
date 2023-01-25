47f1551926bd25a6610aff907ed7e57732769617
Marcus Piri <zerogranit@hotmail.se>
2023-01-25 22:03:00 +0100

    Cleaned

--- src/components/GuestCard/GuestCard.style.tsx
+++ src/components/GuestCard/GuestCard.style.tsx
@@ -4,36 +4,39 @@ export const CardContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    gap: 10px;
-    background: #eff2ee;
-    width: 100%;
-    margin-bottom: 20px;
-    padding: 20px 20px;
-    box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
-    backdrop-filter: blur(2px);
-    border-radius: 15px;
     height: auto;
+    width: 100%;
+    gap: 10px;
+    padding: 20px 20px;
+    margin-bottom: 20px;
+    border-radius: 15px;
+    box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
+    background: #eff2ee;
+    backdrop-filter: blur(2px);
 `;
+
 export const CardWrapper = styled.div`
     display: flex;
     align-items: stretch;
     justify-content: space-between;
     gap: 5.3rem;
 `;
+
 export const CardColumnLeft = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
 `;
+
 export const CardImage = styled.img`
     max-width: 75px;
-    object-fit: cover;
     border-radius: 15px;
+    object-fit: cover;
 `;
 
 export const CardColumnRight = styled.div`
     display: flex;
+    justify-content: flex-end;
     flex-direction: column;
     gap: 50px;
-    justify-content: flex-end;
 `;
