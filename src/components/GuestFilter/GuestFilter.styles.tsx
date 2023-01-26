68c164f2bb6baa1ddaee84da6a83372cb1c40c37
Sandy <sandy.congreve@cmeducations.se>
2023-01-26 11:55:22 +0100

    Added new components to seperate content in contaier

--- src/components/GuestFilter/GuestFilter.styles.tsx
+++ src/components/GuestFilter/GuestFilter.styles.tsx
@@ -8,10 +8,21 @@ export const CardContainer = styled.div`
     box-shadow: 9px 15px 10px -1px rgba(92, 130, 122, 0.28);
     backdrop-filter: blur(2px);
     border-radius: 15px;
-    padding: 5px 10px;
+    gap: 2px;
+    padding: 15px 10px;
     margin: 15px 5px;
 `;
 
+export const OptionText = styled.span`
+    font-size: 14px;
+    color: rgb(239, 138, 98);
+    font-weight: 800;
+    text-decoration: none;
+    
+`;
+export const ContentContainer = styled.div`
+
+`;
 export const Checkbox = styled.input`
     border-radius: 0.15em;
     border: 0.15em solid currentColor;
